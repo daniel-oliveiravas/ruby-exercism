db1b210e867f3663b0294ac320095c38c492d3f9
Daniel Vasconcelos <daniel.olivervas@gmail.com>
2017-03-03 12:47:09 -0300

    robot_name solution

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -3,39 +3,35 @@ module BookKeeping
 end
 
 class Robot
-
+  
   attr_reader :name
-
+  
   $names = []
-
+  
   def initialize
     @name = generate_name
-    $names += @name
   end
-
+  
   def reset
-    $names -= @name
+    $names.delete @name
     @name = generate_name
-    NAMES.push(@name)
   end
-
+  
   private
   def generate_name
-    robot_name = generate_letters + generate_numbers
-    print "robot_name = #{robot_name} and names = #{$names}"
-    until $names.include? robot_name
-      robot_name = generate_letters + generate_numbers
-    end
+    robot_name = ''
+    robot_name = generate_letters + generate_numbers while $names.include? robot_name
+    $names.push robot_name
     robot_name
   end
-
+  
   def generate_letters
     letters = ('A'..'Z').to_a
-    letters[rand(26)] + letters[rand(26)]
+    letters.sample + letters.sample
   end
-
+  
   def generate_numbers
     rand(100..999).to_s
   end
-
+  
 end
